eca5770085eb917e5a4dcddc381cc9e1bf4aa433
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-05-29 14:41:08 +0200

    no ordering

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ d3.json("data/data.json")
     // Pie creation
     const pie = d3.pie()
     //.sort((a, b) => a.y - b.y)
+    .sort(null)
     .value(function(d){return d.x;}); // same as .value(d => d.x)
     //.value(d => valScale(d.x));
 
